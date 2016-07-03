2b6b065363f2c3d69b744a279d6619be76ee9eee
ThomasGri <th.grimmi@yahoo.de>
2016-07-03 12:38:53 +0200

    First commit
    Camera setup+bugs

---
+++ Camera setup/Assets/Skripts/scriptButtonQuit.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scriptButtonQuit : MonoBehaviour {
+
+	public void onClick(){
+			Application.Quit ();
+}
+}
---
+++ Camera setup/Assets/Skripts/scriptMyProjectsButton.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scriptMyProjectsButton : MonoBehaviour {
+
+	public void onClick(){
+
+	}
+}
