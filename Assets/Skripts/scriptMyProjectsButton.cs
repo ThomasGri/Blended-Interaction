3b5729839b5b416aa28a16a38714ef7f054ec780
ThomasGri <th.grimmi@yahoo.de>
2016-07-04 16:45:54 +0200

    Fix camera

--- Assets/Skripts/scriptMyProjectsButton.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class scriptMyProjectsButton : MonoBehaviour {
-
-	public void onClick(){
-
-	}
-}
---
+++ Assets/Skripts/test.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		TangoPointCloud pc = GameObject.FindObjectOfType<TangoPointCloud>();
+		Camera cam = Camera.main;
+
+		if (Input.touchCount == 1)
+		{
+			Touch t = Input.GetTouch(0);
+			Vector3 planeCenter;
+			Plane plane;
+			if (pc.FindPlane(cam, t.position, out planeCenter, out plane))
+			{
+				Debug.Log("Found plane");
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
